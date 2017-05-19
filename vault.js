ee04c85bcf835169b73f3fbd1ba4fee3d2cf0b73
Ross Wu <RAWSE-DA-BAWSE@Rosss-MacBook-Pro.local>
2017-05-18 21:07:56 -1000

    done

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
+    var vault = {}; // empty object
+
+    var setValue = function(key, value){
+        vault[key]=value; // set object key and assign it the same values
+
+
+    };
+
+    var getValue = function (key){
+        if (vault[key] !== undefined){ // make sure the values that come in has a value 
+            return vault[key]; // returns the value "key" in the vault object
+
+        }else{
+            return null;
+        }
+    };
+
+     return { setValue: setValue,
+                getValue: getValue
+            }
+
   
 };
+
